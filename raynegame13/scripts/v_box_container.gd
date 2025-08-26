90919f2bc00ead1cde15cd71225109253b59150d
Darbyray <127260578+Darbyray@users.noreply.github.com>
2025-08-26 12:22:49 +1200

    ladder and collectable work
    ive made some progress on the ladder and the collectables working. they dotn ork at all its literally just the frame work, the ladder may need to be scrapped due to the videos i have being useless

--- raynegame13/scripts/v_box_container.gd
+++ raynegame13/scripts/v_box_container.gd
@@ -7,3 +7,16 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(WORLD)
 func _on_leave_pressed() -> void:
 		get_tree().quit()
+
+
+var on_ladder: bool= false 
+
+func movement(delta: float ) -> void:
+	pass
+
+func _ladder_detect():
+	pass
+
+
+func ladder_movement(delta: float):
+	pass
